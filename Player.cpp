b7a8efee2cd156508c4976b26b30911ff776f12d
GodArthur <korjon0000@gmail.com>
2023-02-04 18:36:14 -0500

    Added player and player driver files with content

---
+++ Player.cpp
@@ -0,0 +1,30 @@
+#include "Player.h"
+
+
+class Player{
+
+	/*
+	Player::Player(Territory* territories, Hand hand) {
+
+		Player::territories = territories;
+		Player::hand = hand;
+
+		
+	}*/
+
+
+	/* Territory* toAttack(){
+	* 
+	* Territory t[] = {"Spain, Germany, Scandanavia, France"}
+	* return t;
+	* }
+	*/
+
+	/* Territory* toDefend(){
+	*
+	* Territory t[] = {"China, Vietnam, Japan, Korea"}
+	* return t;
+	* }
+	*/
+
+};
