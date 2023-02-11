9714ef9a7d4d7b8d810fd73ac7a8bc906d23f399
Tristan Judd <tristan.lueger@gmail.com>
2023-02-10 21:43:49 -0500

    Added some method definitions to my classes

--- Cards.cpp
+++ Cards.cpp
@@ -1,7 +1,8 @@
-#include "Card.h"
+#include "Cards.h"
 #include <cstdlib>
 #include <chrono>
 
+// Card class method definitions
 Card::Card() {
 	srand(time(NULL));
 	cardType = (CardType)(rand() % 5);
@@ -15,3 +16,41 @@ CardType Card::getType() {
 	return cardType;
 }
 
+void Card::play() {
+	// TODO:
+	// create order and add it to the list of orders then return card to deck
+}
+
+// Hand class method declarations
+Hand::Hand() {
+	
+}
+
+std::vector<Card> Hand::getHand() {
+	return contents;
+}
+
+void Hand::insert(Card card) {
+	contents.push_back(card);
+}
+
+void Hand::remove(Card card) {
+	int index = -1;
+
+	for (Card c : contents) {
+		if
+	}
+}
+
+// Deck class method delcarations 
+Deck::Deck() {
+
+}
+
+Card Deck::draw() {
+	// TODO
+}
+
+void Deck::insert(Card card) {
+	// TODO
+}
--- Cards.h
+++ Cards.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
+#include <vector>
 
+
+// Enum of the different card types
 enum CardType
 {
 	bomb,
@@ -10,6 +13,8 @@ enum CardType
 	diplomacy
 };
 
+// Card class header
+// Card is an object containing details about a specific instance of a Warzone card
 class Card {
 private:
 	CardType cardType;
@@ -18,4 +23,29 @@ public:
 	Card(CardType t);
 	void play();
 	CardType getType();
+};
+
+// Hand class header
+// A Hand object is a collection of Card objects
+class Hand {
+private:
+	std::vector<Card> contents;
+
+public:
+	Hand(); // Default constructor with no cards
+	std::vector<Card> getHand(); // Returns the player's hand as a vector of Card objects
+	void insert(Card card); // Inserts a card into the contents vector
+	void remove(Card card); // Used when a card is played, removes card from hand
+
+};
+
+// Deck class header
+// A deck is a collection of Card objects
+class Deck {
+private:
+	std::vector<Card> contents;
+public:
+	Deck();
+	Card draw();
+	void insert(Card card);
 };
