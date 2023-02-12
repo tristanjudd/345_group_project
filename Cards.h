0b0e8030acbc18c8799251a64fc0616683e8543d
Tristan Judd <tristan.lueger@gmail.com>
2023-02-12 12:31:45 -0500

    finished self-contained features for my part

--- Cards.h
+++ Cards.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <string>
 #include <vector>
-
+// #include "Player.h"
 
 // Enum of the different card types
 enum CardType
@@ -13,14 +13,19 @@ enum CardType
 	diplomacy
 };
 
+class Hand;
+
 // Card class header
 // Card is an object containing details about a specific instance of a Warzone card
 class Card {
 private:
 	CardType cardType;
+	Hand* hand;
 public:
 	Card();
 	Card(CardType t);
+	Card(Hand* h);
+	void assignHand(Hand* hand);
 	void play();
 	CardType getType();
 };
@@ -29,13 +34,14 @@ public:
 // A Hand object is a collection of Card objects
 class Hand {
 private:
-	std::vector<Card> contents;
+	/*Player* owner;*/
+	std::vector<Card *> contents;
 
 public:
 	Hand(); // Default constructor with no cards
-	std::vector<Card> getHand(); // Returns the player's hand as a vector of Card objects
-	void insert(Card card); // Inserts a card into the contents vector
-	void remove(Card card); // Used when a card is played, removes card from hand
+	std::vector<Card *> getHand(); // Returns the player's hand as a vector of Card objects
+	void insert(Card* card); // Inserts a card into the contents vector
+	void remove(Card* card); // Used when a card is played, removes card from hand
 
 };
 
@@ -43,9 +49,16 @@ public:
 // A deck is a collection of Card objects
 class Deck {
 private:
-	std::vector<Card> contents;
+	int count;
+	std::vector<Card *> contents;
 public:
 	Deck();
-	Card draw();
-	void insert(Card card);
+	Deck(int n);
+	Card* draw();
+	void insert(Card* card);
+	void shuffle();
+	int size();
+	Card* peek(int n);
 };
+
+bool cardTypeSame(Card* c1, Card* c2);
