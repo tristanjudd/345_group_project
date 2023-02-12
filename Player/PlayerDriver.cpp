5111625a11bde5a26b4c05dc90ab45699ff85998
GodArthur <korjon0000@gmail.com>
2023-02-12 17:09:38 -0500

    Made necessary modifications to the printing of playerDriver

--- Player/PlayerDriver.cpp
+++ Player/PlayerDriver.cpp
@@ -6,16 +6,19 @@ int playerDriver(){
 
     Player *p = new Player();
 
+    cout << endl;
+
     cout << "Generating Territories to attack" << endl;
     vector<Territory*>* attacking = p->toAttack();
 
+    cout << endl;
     cout << "Generating Territories to defend" << endl;
     vector<Territory*>* defending = p->toDefend();
 
     cout << "Issuing an order" << endl;
     p->issueOrder();
 
-    cout << "Player: " << *p << endl;
+    cout << endl;
 
     cout << "Defending Territories:" << endl;
 
@@ -24,14 +27,19 @@ int playerDriver(){
         cout << *defending->at(i) << endl;
     }
 
-    cout << "Defending Territories:" << endl;
+    cout << "Attacking Territories:" << endl;
 
     for (int i = 0; i < attacking->size(); i++){
 
         cout << *attacking->at(i) << endl;
     }
 
-    cout << "added Order: " << *p->getOrders()->at(0);
+    cout << "added Order: " << *p->getOrders()->at(0) << endl;
+
+    cout << endl;
+
+
+    cout << "Player: " << endl << *p << endl;
 
     cout << endl;
 
