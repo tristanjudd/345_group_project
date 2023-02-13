bf4e0a3cf663ba846ff8365551fbcc470a4e890f
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-02-12 19:32:05 -0500

    Game template

--- GameEngine/GameEngineDriver.cpp
+++ GameEngine/GameEngineDriver.cpp
@@ -6,7 +6,22 @@ int gameEngineDriver() {
     GameEngine::start();
    // mapDriver();
     GameEngine::addPlayers();
-
-
-
+    cout << "Assigning countries to players" << endl;
+    int *winner = new int(-1);
+    while (*winner == -1) {
+        cout << "Assign reinforcements to countries" << endl;
+        cout << "Issuing orders" << endl;
+        cout << "Executing orders" << endl;
+        *winner = 0;
+    }
+    cout << "Player " << *winner << " wins!" << endl;
+    cout << "Game Over" << endl;
+    cout << "Play again? (y/n): ";
+    char playAgain;
+    cin >> playAgain;
+    if (playAgain == 'y') {
+        gameEngineDriver();
+    }
+    cout << "Goodbye!" << endl;
+    return 0;
 }
