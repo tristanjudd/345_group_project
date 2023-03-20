7daf78857d7f8f0f86365c0cc924a174ed6c353d
GodArthur <korjon0000@gmail.com>
2023-03-19 21:16:58 -0400

    fixed another bug

--- main.cpp
+++ main.cpp
@@ -10,22 +10,22 @@ int main(int argc, char *argv[]) {
     std::cout << "Welcome to our bootleg Warzone!" << std::endl;
 
     // -file /home/jay/Projects/345_group_project/CommandProcessing/test.cmd
-    try {
-        if (string(argv[1]) == "-console") {
-            gameEngineDriver(GAME_MODE::console, "");
-        } else if (string(argv[1]) == "-file"){
-            gameEngineDriver(GAME_MODE::file, argv[2]);
-        } else {
-            throw std::runtime_error("Invalid command line arguments.");
-        }
-    } catch (const std::exception& e) {
-        cout << "[ERROR]: " << e.what() << "... Exiting program.";
-        return -1;
-    }
-    
+//    try {
+//        if (string(argv[1]) == "-console") {
+//            gameEngineDriver(GAME_MODE::console, "");
+//        } else if (string(argv[1]) == "-file"){
+//            gameEngineDriver(GAME_MODE::file, argv[2]);
+//        } else {
+//            throw std::runtime_error("Invalid command line arguments.");
+//        }
+//    } catch (const std::exception& e) {
+//        cout << "[ERROR]: " << e.what() << "... Exiting program.";
+//        return -1;
+//    }
+
 
     //PART2
-    //logDriver(argc, argv[]);
+    logDriver(argc, argv);
 
 
     return 0;
