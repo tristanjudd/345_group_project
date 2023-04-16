b3759b6bd7c180e7ea9e8dcc2d4fed978b7fada7
Anh TU <anhtuchau99@gmail.com>
2023-04-16 16:26:52 -0400

    Removed commented code in Orders

--- Orders/Orders.cpp
+++ Orders/Orders.cpp
@@ -306,20 +306,6 @@ bool Advance::execute() {
             // battle between 2 armies
         else {
 
-//            //check if target is a neutral strategy
-//            bool isNeutral = false;
-//            if (typeid(target->getOwner()->getStrategy()) == typeid(Neutral)) {
-//                isNeutral = true;
-//            }
-//
-//            if (isNeutral) {
-//                //change target player strategy to aggressive
-//                delete target->getOwner()->getStrategy();
-//                PlayerStrategy *aggressive = new Aggressive(target->getOwner());
-//                target->getOwner()->setStrategy(aggressive);
-//                cout << "DEBUG: Neutral player strategy changed to aggressive" << endl;
-//            }
-
             //check if target is a neutral strategy
             Neutral* neutralCheck = dynamic_cast<Neutral*>(target->getOwner()->getStrategy());
 
