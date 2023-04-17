12bbb0dd293edf7aede21fd9e55d5de93b54b984
Michael Roubeiz <michaelroubeiz@gmail.com>
2023-04-16 21:07:12 -0400

    Update destructor

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -63,16 +63,14 @@ ostream &operator<<(ostream &out, const GameEngine &gameEngine) {
 //destructor
 GameEngine::~GameEngine() {
     cout << "GameEngine destructor called" << endl;
-    winner = nullptr;
-    map = nullptr;
-    players = nullptr;
-    currentTurn = nullptr;
-    maxTurns = nullptr;
     delete winner;
     delete map;
-    delete players;
     delete currentTurn;
-    delete maxTurns;
+
+    for (auto player: *players) {
+        delete player;
+    }
+    delete players;
 
     delete neutral;
     delete peaceStatus;
@@ -1004,7 +1002,8 @@ int GameEngine::loadTournament(string arguments) {
             // run pre validation of map
             bool mapLoaded = MapLoader::loadMap(this, new string(mapFileString));
             if (!mapLoaded) {
-                cout << "Tournament command invalid: Incorrect map file " << mapFileString << " Please try again!" << endl;
+                cout << "Tournament command invalid: Incorrect map file " << mapFileString << " Please try again!"
+                     << endl;
                 return -1;
             }
 
